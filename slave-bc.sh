2ce72afe2de4eba114bc944f2672c54199f47a13
UtochkaJZX100 <artmail0451@gmail.com>
2025-08-22 13:43:02 +0300

    Slave

--- slave-bc.sh
+++ slave-bc.sh
@@ -4,14 +4,14 @@ cp -f /home/otus/mysqlds.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
 
 #Замена сохранённого конфига
 
-mysql -u root -p  -e STOP SLAVE;
+mysql -u root -e STOP SLAVE;
 
 #Остановка репликации
 
-mysql -u root -p  -e CHANGE REPLICATION SOURCE TO SOURCE_HOST='192.168.10.225', SOURCE_USER='otus', SOURCE_PASSWORD='qwerty', SOURCE_AUTO_POSITION=1, GET_SOURCE_PUBLIC_KEY=1;
+mysql -u root -e CHANGE REPLICATION SOURCE TO SOURCE_HOST='192.168.10.225', SOURCE_USER='otus', SOURCE_PASSWORD='qwerty', SOURCE_AUTO_POSITION=1, GET_SOURCE_PUBLIC_KEY=1;
 
 #Настройка параметров для репликации
 
-mysql -u root -p  -e START REPLICA;
+mysql -u root -e START REPLICA;
 
 #Запуск репликации

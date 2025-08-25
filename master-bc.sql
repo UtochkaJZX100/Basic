a7276df09e1c58425210700cd5f4f5777eac8fb8
UtochkaJZX100 <artmail0451@gmail.com>
2025-08-25 14:05:40 +0300

    Master

--- master-bc.sql
+++ master-bc.sql
@@ -1,7 +1,7 @@
-mysql -u root -e CREATE USER otus@'%' IDENTIFIED WITH 'caching_sha2_password' BY 'qwerty';
+CREATE USER otus@'%' IDENTIFIED WITH 'caching_sha2_password' BY 'qwerty';
 
 #Создание пользователя для репликации
 
-mysql -u root -e GRANT REPLICATION SLAVE ON *.* TO otus@'%';
+GRANT REPLICATION SLAVE ON *.* TO otus@'%';
 
 #Добавляем пользователю для репликации права для подключения с любого хоста
